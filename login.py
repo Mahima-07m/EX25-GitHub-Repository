6da3984a56963e0efad0c50fc08c6eba924a6dde
Mahima <mahimaprabhakaran05@gmail.com>
2026-08-10 11:26:51 +0530

    Add login function

--- login.py
+++ login.py
@@ -1,4 +1,2 @@
 def login(username, password):
-    if username == "admin" and password == "1234":
-        return "Login successful"
-    return "Invalid username or password"
+    return username == "admin" and password == "1234"
